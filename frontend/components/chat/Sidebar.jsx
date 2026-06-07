0a2d82a9e3143775234ca1fc786ee5d5fbc5cb23
Jaiv Patel <jaivpatel402@gmail.com>
2026-06-07 14:01:10 +0530

    solve error

--- frontend/components/chat/Sidebar.jsx
+++ frontend/components/chat/Sidebar.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react"
-import { Search, PenSquare, MonitorSmartphone } from "lucide-react"
 import useChatStore from "../../src/store/useChatStore"
 import useAuthStore from "../../src/store/useAuthStore"
 import { getSocket } from "../../lib/socket"
