a0b56080ae4e8d95fc3fd909eaf200ba3b1eb64e
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-06-09 20:50:12 +0530

    add the github authentication

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,30 @@
-import express from "express";
-import protectRoute from "../middleware/auth.middleware.js";
-import { validateSignup, validateLogin } from "../middleware/validate.js";
-import { signup, login, logout, googleAuth, updateProfile, updateProfilePicture, checkAuth, subscribeToPush } from "../controllers/auth.controller.js";
+import express from 'express'
+import protectRoute from '../middleware/auth.middleware.js'
+import { validateSignup, validateLogin } from '../middleware/validate.js'
+import {
+  signup,
+  login,
+  logout,
+  googleAuth,
+  updateProfile,
+  updateProfilePicture,
+  checkAuth,
+  subscribeToPush,
+  githubRedirect,
+} from '../controllers/auth.controller.js'
+import { githubCallback } from '../controllers/githubCallback.controller.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/signup", validateSignup, signup);
-router.post("/login", validateLogin, login);
-router.post("/google", googleAuth);
-router.post("/logout", logout);
-router.put("/update-profile", protectRoute, updateProfile);
-router.put("/update-profile-picture", protectRoute, updateProfilePicture);
-router.get("/check", protectRoute, checkAuth);
-router.post("/push-subscribe", protectRoute, subscribeToPush);
+router.post('/signup', validateSignup, signup)
+router.post('/login', validateLogin, login)
+router.post('/google', googleAuth)
+router.get('/github', githubRedirect)
+router.get('/github/callback', githubCallback)
+router.post('/logout', logout)
+router.put('/update-profile', protectRoute, updateProfile)
+router.put('/update-profile-picture', protectRoute, updateProfilePicture)
+router.get('/check', protectRoute, checkAuth)
+router.post('/push-subscribe', protectRoute, subscribeToPush)
 
-export default router;
+export default router
